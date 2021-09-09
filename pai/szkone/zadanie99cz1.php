a1e0cb996fe51193febdae555ef7d5f55bf09628
SitarzStefan <90000900+SitarzStefan@users.noreply.github.com>
2021-09-09 20:29:42 +0200

    Add files via upload

---
+++ pai/szkone/zadanie99cz1.php
@@ -0,0 +1,30 @@
+<?php
+echo'ZSŁ<br>';
+$name="Janusz";
+echo 'Imię:$<br>';
+//całkowity 
+$całkowita=10;
+$bin=0b101; //5
+$oct=011;   //9
+$hex=0x11    //17
+echo $hex;
+//zmiennoprzecinkwa
+$prawda=true;
+$fałsz=false; //nie używać polsich zaków w nazwie zmiennych
+echo $prawda;
+//składnia heredoc 
+echo <<< E
+Twoje imię: $name<br>
+E;
+$surname="Noak"
+echo "\$name $surname";
+
+
+
+
+
+
+
+
+
+?>
---
+++ pai/szkone/zadanie99cz2.php
@@ -0,0 +1,16 @@
+<?php
+echo PHP_VERSION;
+echo 2**10;
+$x=10;
+$y=1;
+echo $x<=>$y;
+// równe / identyczne
+$x=1;
+$y=1.0;
+echo gettype($x) //integer
+echo gettype($y) //duble
+if ($x==$y)
+(echo "równe");
+
+
+?>
