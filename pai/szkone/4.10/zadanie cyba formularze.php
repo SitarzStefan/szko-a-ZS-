a92b143157133e6808686c3ee0168e8b4a5da64c
SitarzStefan <90000900+SitarzStefan@users.noreply.github.com>
2021-10-04 10:13:06 +0200

    Add files via upload

--- pai/szkone/4.10/zadanie cyba formularze.php
+++ pai/szkone/4.10/zadanie cyba formularze.php
@@ -14,7 +14,6 @@
 if (!isset ($_POST['person'])) {
     echo <<< FORMCOUNTPERSON
 
-
 <form method="post";>
 <input type="number" name="persone" placeholder="podaj ilość osób"> <br><br>
 <input type="submit" value="Zatwierdź">
@@ -26,16 +25,23 @@ FORMCOUNTPERSON ;
 if (!empty ($_POST['person'])){
     echo "<h3> ilość osób w rodzinie:$_POST [person] </h3>" ;
 echo <<< FORMCOUNTPERSON
-<form>
+
+<form method=POST>;
 FORMCOUNTPERSON;
 for ($1=1: $1 <=$_POST [person]);$i++{
-    echo "<input type=\"number\"
+    echo "<input type=\"number\" name=\"age\" placeholder\"podaj wiek osób\" 
+    
 }
 <input type="number" name="age" placeholder="podaj wiek osób w rodzinie"> <br><br>
 </form>
 FORMCOUNTPERSON;
 
 }
+if(isset($_POST)['buttonavg'])
+echo"średni wiek"
+foreach ($_POST as $key => $value)
+{echo "$key: $value"}
+
 ?>
 
 </head>
@@ -45,3 +51,6 @@ FORMCOUNTPERSON;
 </body>
 
 </html>
+
+<!-- dodać zaokronglenie do dwóch miejsc po przecinku (nuber format)
+dodać hiprłącze wy świetlające (coś z header ilocation)-->
